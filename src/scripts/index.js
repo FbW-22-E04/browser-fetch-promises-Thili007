3e5527ba6ca64e9bcf548ef9fb087b41ea604edb
Thili007 <tpunchihewage@gmail.com>
2022-09-28 16:00:41 +0200

    added files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,18 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+import "../styles/main.scss";
 // Import any additional modules you want to include below \/
-
 
 // \/ All of your javascript should go here \/
 
+new Promise(() =>
+  setTimeout(() => {
+    document.querySelector(".modal").style.display = "block";
+  }, 6000)
+);
+
+document
+  .querySelector(".close")
+  .addEventListener(
+    "click",
+    () => (document.querySelector(".modal").style.display = "none")
+  );
